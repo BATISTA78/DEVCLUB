34f3e960b272b18fae082f86bf357fc7490c0c46
batista78 <contatojb78@hotmail.com>
2024-03-27 16:40:07 -0300

    Meu primeiro commit

---
+++ JAVASCRIPT/scripts.js
@@ -0,0 +1,34 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")// Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value")// Outras Moedas
+    
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
+ 
+    if(currencySelect.value == "dolar"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            Style: "currency",
+            currency: "USD",
+        }).format(inputCurrencyValue / dolarToday)
+    }   
+    if(currencySelect.value == 'euro'){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('de-DE', {
+            Style: "currency",
+            currency: "EUR",
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        Style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)  
+
+    
+}
+convertButton.addEventListener("click", convertValues)
+    
